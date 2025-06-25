f0a24b72347c795260426ce9a2cbb33289394624
Carlos Daniel Sucre <143762705+CarlosDanielSucre@users.noreply.github.com>
2025-06-24 22:16:50 -0300

    Update and rename sql_basics to sql_window_functions.sql

---
+++ sql_window_functions.sql
@@ -0,0 +1,11 @@
+--1. view the table
+
+--2. order by popularity
+
+--3. add a popularity column
+
+--4. try different functions
+
+--5. try different windows
+
+--6. what are the top 3 most popular names for each gender
