8dc923060f1a7823894b364b8451054f9f6a20e0
Carlos Daniel Sucre <143762705+CarlosDanielSucre@users.noreply.github.com>
2025-06-25 23:11:48 -0300

    Update challenge_01.sql

--- challenges/challenge_01.sql
+++ challenges/challenge_01.sql
@@ -1,1 +1,7 @@
-HERE I LL WHRITE THE RESPONSE
+SELECT   employee_id,
+         department_id,
+         salary,
+    ROW_NUMBER() OVER(PARTITION BY department_id ORDER BY salary DESC) as rank_salary,
+    MAX(salary) OVER(PARTITION BY department_id) AS max_salary,
+    LAG(salary) OVER (PARTITION BY department_id ORDER BY salary DESC) AS prev_salary
+FROM employees;
